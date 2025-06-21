d0ff563f5cff1eb5655baa0c3fe5210800046d51
Mohamed <57944576+saksooka@users.noreply.github.com>
2025-06-21 15:36:52 +0300

    Update rtx5090 flux.sh

--- rtx5090 flux.sh
+++ rtx5090 flux.sh
@@ -116,7 +116,6 @@ huggingface-cli download ali-vilab/ACE_Plus subject/comfyui_subject_lora16.safet
 huggingface-cli download ali-vilab/ACE_Plus portrait/comfyui_portrait_lora64.safetensors --local-dir ${COMFYUI_DIR}/models/loras/
 huggingface-cli download ali-vilab/ACE_Plus local_editing/comfyui_local_lora16.safetensors --local-dir ${COMFYUI_DIR}/models/loras/
 huggingface-cli download jackzheng/flux-fill-FP8 fluxFillFP8_v10.safetensors --local-dir ${COMFYUI_DIR}/models/diffusion_models/
-huggingface-cli download facebook/sam2.1-hiera-large sam2.1_hiera_large.pt --local-dir ${COMFYUI_DIR}/
 
 
 # Packages are installed after nodes so we can fix them...
